c756a68dca232ca56a949d1ee14603ea77370e2d
an-seunghwan <dpeltms79@gmail.com>
2024-07-29 22:39:17 +0900

    [Update] add disclaimer

--- streamlit.py
+++ streamlit.py
@@ -52,6 +52,23 @@ def main():
     
     st.markdown("""---""")
     st.write("""
+    ###### Disclaimer
+        The cryptocurrency price prediction model provided on this website is for informational purposes only and does not constitute financial advice. 
+        The predictions and analysis are based on historical data and do not guarantee future performance. 
+        Investing in cryptocurrencies involves significant risk, and you should be aware that you could lose some or all of your investment.
+        We are not responsible for any financial losses that may result from your investment decisions. 
+        You are solely responsible for your own investment choices.         
+    
+    ###### 책임 고지
+        이 웹사이트에서 제공하는 가상화폐 가격 예측 모형은 정보 제공 목적으로만 사용되며 재정적 조언을 구성하지 않습니다. 
+        예측 및 분석은 과거 데이터를 기반으로 하며 미래 성과를 보장하지 않습니다. 
+        가상화폐 투자에는 상당한 위험이 따르며, 투자한 금액의 일부 또는 전부를 잃을 수 있음을 인지해야 합니다. 
+        본 웹사이트와 제작자는 귀하의 투자 결정으로 인해 발생할 수 있는 재정적 손실에 대해 책임지지 않습니다. 
+        투자의 책임은 전적으로 귀하에게 있습니다.
+    """)
+    
+    st.markdown("""---""")
+    st.write("""
     If you are interested in our results, please visit our GitHub!
     - [https://github.com/Optim-Lab/CryptoVAE](https://github.com/Optim-Lab/CryptoVAE)
     
