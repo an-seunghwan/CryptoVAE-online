38e2249ed96e1ddf0e2b564656a81c9bf9196491
an-seunghwan <dpeltms79@gmail.com>
2024-08-09 12:28:10 +0900

    Automated update on 2024-08-09

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Navigate to project directory
-cd /Users/anseunghwan/Documents/GitHub/CryptoVAE-online
+# cd /Users/anseunghwan/Documents/GitHub/CryptoVAE-online
+cd /Users/an-seunghwan/Documents/GitHub/CryptoVAE-online
 
 # build train dataset
-/opt/anaconda3/envs/crypto/bin/python dataset.py 
+# /opt/anaconda3/envs/crypto/bin/python dataset.py 
+/opt/anaconda3/envs/deep/bin/python dataset.py 
 
 # train model
-/opt/anaconda3/envs/crypto/bin/python main.py --model GLD_finite 
+# /opt/anaconda3/envs/crypto/bin/python main.py --model GLD_finite 
+/opt/anaconda3/envs/deep/bin/python main.py --model GLD_finite 
 
 # Git commit and push
 git add .
